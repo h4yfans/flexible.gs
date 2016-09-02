4f1655cce960e2f4f633e54da7675e0870a1191b
dnomak <me@dnomak.com>
2016-09-02 23:27:42 +0300

    sidebar learn link update

--- app/component/partial/Sidebar.react.js
+++ app/component/partial/Sidebar.react.js
@@ -7,68 +7,45 @@ const queryString = require('query-string');
 
 var Sidebar = React.createClass({
 
-  getInitialState: function() {
-    return {
-      type: "css",
-      menuType: "CSS",
-      menuDescription: this.props.common.staticCompiled
-    };
-  },
-
-  componentWillMount: function() {
-    const parsed = queryString.parse(location.search);
-
-    if(parsed.type == "scss" || parsed.type == "sass" || parsed.type == "less" || parsed.type == "stylus") {
-      this.setState({ menuDescription: this.props.common.dynamicCore });
-    }
-    else if (parsed.type == "css" || parsed.type == "bem") {
-      this.setState({ menuDescription: this.props.common.staticCompiled });
-    }
-
-    if (parsed.type == "scss") {
-      this.setState({
-        type: parsed.type,
-        menuType: "Scss"
-      });
-    }
-    else if (parsed.type == "sass") {
-      this.setState({
-        type: parsed.type,
-        menuType: "Sass"
-      });
-    }
-    else if (parsed.type == "less") {
-      this.setState({
-        type: parsed.type,
-        menuType: "Less"
-      });
-    }
-    else if (parsed.type == "stylus") {
-      this.setState({
-        type: parsed.type,
-        menuType: "Stylus"
-      });
-    }
-    else if(parsed.type == "css") {
-      this.setState({
-        type: parsed.type,
-        menuType: "CSS"
-      });
-    }
-    else if (parsed.type == "bem") {
-      this.setState({
-        type: parsed.type,
-        menuType: "BEM"
-      });
-    }
-  },
-
   render: function() {
 
     var inputEmailClass = "xl-1-1 xl-db xl-lh56 xl-h56 xl-ba-white xl-bw2 xl-bo-green-300 xl-brl8 xl-ffscp xl-pl16 lg-pl8 md-pl16";
     var inputSubmitClass = "xl-1-1 xl-db xl-lh56 xl-h56 xl-co-white xl-ba-green-300 xl-brr8";
 
     var mailchimpForm = '<form class="wrap" action="//flexible.us8.list-manage.com/subscribe/post?u=0cfb9d416ef45eba73b6368b6&amp;id='+ this.props.mailchimp.id +'" method="post" name="mc-embedded-subscribe-form" target="_blank"><div class="col xl-8-12"><input type="email" name="EMAIL" class="'+ inputEmailClass +'" placeholder="'+ this.props.mailchimp.emailAddress +'" /><div style="position: absolute; left: -5000px;"><input type="text" name="'+ this.props.mailchimp.name +'" tabindex="-1"/></div></div><div class="col xl-4-12"><input type="submit" class="'+ inputSubmitClass +'" value="'+ this.props.mailchimp.subscribe +'" name="subscribe" /></div></form>';
+
+    const parsed = queryString.parse(location.search);
+
+    var linkWrap = "/learn/wrap?type=" + parsed.type;
+    var linkCol = "/learn/col?type=" + parsed.type;
+    var linkColRow = "/learn/col-row?type=" + parsed.type;
+    var linkHidden = "/learn/hidden?type=" + parsed.type;
+    var linkNotHidden = "/learn/not-hidden?type=" + parsed.type;
+    var linkGutter = "/learn/gutter?type=" + parsed.type;
+    var linkOutside = "/learn/outside?type=" + parsed.type;
+    var linkWrapCol = "/learn/wrap-col?type=" + parsed.type;
+    var linkAuto = "/learn/auto?type=" + parsed.type;
+    var linkTable = "/learn/table?type=" + parsed.type;
+    var linkWidth = "/learn/width?type=" + parsed.type;
+    var linkFlexbox = "/learn/flexbox?type=" + parsed.type;
+    var linkNormal = "/learn/normal?type=" + parsed.type;
+    var linkLeft = "/learn/left?type=" + parsed.type;
+    var linkCenter = "/learn/center?type=" + parsed.type;
+    var linkRight = "/learn/right?type=" + parsed.type;
+    var linkTop = "/learn/top?type=" + parsed.type;
+    var linkMiddle = "/learn/middle?type=" + parsed.type;
+    var linkBottom = "/learn/bottom?type=" + parsed.type;
+    var linkBetween = "/learn/between?type=" + parsed.type;
+    var linkAround = "/learn/around?type=" + parsed.type;
+    var linkBaseline = "/learn/baseline?type=" + parsed.type;
+    var linkFirst = "/learn/first?type=" + parsed.type;
+    var linkNotFirst = "/learn/not-first?type=" + parsed.type;
+    var linkLast = "/learn/last?type=" + parsed.type;
+    var linkNotLast = "/learn/not-last?type=" + parsed.type;
+    var linkReverse = "/learn/reverse?type=" + parsed.type;
+    var linkNotReverse = "/learn/not-reverse?type=" + parsed.type;
+    var linkMasonry = "/learn/masonry?type=" + parsed.type;
+    var linkBreakpoint = "/learn/breakpoint?type=" + parsed.type;
 
     return (
       <div>
@@ -163,161 +140,181 @@ var Sidebar = React.createClass({
               <div className="xl-ba-gray-100 xl-py8 xl-px16 xl-co-black-500">
                 <div className="wrap xl-flexbox xl-between xl-auto">
                   <div className="col xl-fs14">
-                    {this.state.menuDescription}
+                    {(() => {
+                      switch (parsed.type) {
+                        case "scss": return this.props.common.dynamicCore;
+                        case "sass": return this.props.common.dynamicCore;
+                        case "less": return this.props.common.dynamicCore;
+                        case "stylus": return this.props.common.dynamicCore;
+                        case "css": return this.props.common.staticCompiled;
+                        case "bem": return this.props.common.staticCompiled;
+                        default: return this.props.common.staticCompiled;
+                      }
+                    })()}
                   </div>
                   <div className="col xl-fs14 xl-fw600">
-                    {this.state.menuType}
+                    {(() => {
+                      switch (parsed.type) {
+                        case "scss": return "Scss";
+                        case "sass": return "Sass";
+                        case "less": return "Less";
+                        case "stylus": return "Stylus";
+                        case "css": return "CSS";
+                        case "bem": return "BEM";
+                        default: return "CSS";
+                      }
+                    })()}
                   </div>
                 </div>
               </div>
               <div className="xl-pl8 xl-ba-gray-100">
                 <Link
-                  to="/learn/wrap"
+                  to={linkWrap}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   wrap
                 </Link>
                 <Link
-                  to="/learn/col"
+                  to={linkCol}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   col
                 </Link>
                 <Link
-                  to="/learn/col-row"
+                  to={linkColRow}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   col-row
                 </Link>
                 <Link
-                  to="/learn/hidden"
+                  to={linkHidden}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   hidden
                 </Link>
                 <Link
-                  to="/learn/not-hidden"
+                  to={linkNotHidden}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   not-hidden
                 </Link>
                 <Link
-                  to="/learn/gutter"
+                  to={linkGutter}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   gutter
                 </Link>
                 <Link
-                  to="/learn/outside"
+                  to={linkOutside}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   outside
                 </Link>
                 <Link
-                  to="/learn/wrap-col"
+                  to={linkWrapCol}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   wrap-col
                 </Link>
                 <Link
-                  to="/learn/auto"
+                  to={linkAuto}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   auto
                 </Link>
                 <Link
-                  to="/learn/table"
+                  to={linkTable}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   table
                 </Link>
                 <Link
-                  to="/learn/width"
+                  to={linkWidth}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   width
                 </Link>
                 <Link
-                  to="/learn/flexbox"
+                  to={linkFlexbox}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   flexbox
                 </Link>
                 <Link
-                  to="/learn/normal"
+                  to={linkNormal}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   normal
                 </Link>
                 <Link
-                  to="/learn/left"
+                  to={linkLeft}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   left
                 </Link>
                 <Link
-                  to="/learn/center"
+                  to={linkCenter}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   center
                 </Link>
                 <Link
-                  to="/learn/right"
+                  to={linkRight}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   right
                 </Link>
                 <Link
-                  to="/learn/top"
+                  to={linkTop}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   top
                 </Link>
                 <Link
-                  to="/learn/middle"
+                  to={linkMiddle}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   middle
                 </Link>
                 <Link
-                  to="/learn/bottom"
+                  to={linkBottom}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   bottom
                 </Link>
                 <Link
-                  to="/learn/between"
+                  to={linkBetween}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   between
                 </Link>
                 <Link
-                  to="/learn/around"
+                  to={linkAround}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   around
                 </Link>
                 <Link
-                  to="/learn/baseline"
+                  to={linkBaseline}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   baseline
                 </Link>
                 <Link
-                  to="/learn/first"
+                  to={linkFirst}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   first
                 </Link>
                 <Link
-                  to="/learn/not-first"
+                  to={linkNotFirst}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   not-first
                 </Link>
                 <Link
-                  to="/learn/last"
+                  to={linkLast}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   last
                 </Link>
                 <Link
-                  to="/learn/not-last"
+                  to={linkNotLast}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   not-last
                 </Link>
                 <Link
-                  to="/learn/reverse"
+                  to={linkReverse}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   reverse
                 </Link>
                 <Link
-                  to="/learn/not-reverse"
+                  to={linkNotReverse}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   not-reverse
                 </Link>
                 <Link
-                  to="/learn/masonry"
+                  to={linkMasonry}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-gray-200 xl-bwb1">
                   masonry
                 </Link>
                 <Link
-                  to="/learn/breakpoint"
+                  to={linkBreakpoint}
                   className="xl-db xl-py8 xl-px16 ho-ba-green-300 ho-co-white ho-bo-green-300 xl-ba-white xl-co-black-500 xl-bo-white xl-bwb1">
                   breakpoint
                 </Link>
